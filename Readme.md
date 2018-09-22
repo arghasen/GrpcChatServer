12f2b5e69247dec55143d1c265b395dfb6d7938a
Argha Sen <iamslowdeath@gmail.com>
2018-09-22 23:31:07 +0530

    add readme

---
+++ Readme.md
@@ -0,0 +1,7 @@
+#GRPC Chat Server.
+
+## Seeding the data 
+The data to be seeded is in the init folder. It can used directly with redis-cli
+```
+./redis-cli < seed.txt
+```
---
+++ src/main/java/com/arghasen/sharechatassignment/server/GrpcServerApp.java
@@ -0,0 +1,10 @@
+package com.arghasen.sharechatassignment.server;
+
+public class GrpcServerApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!!");
+	}
+
+}
